06ee99216887fa4c27e2defe4417e508f839eee0
Fontaine Cook <fontaine.cook@pearance.com>
2011-04-09 03:41:15 -0400

    Removed that check for root in favor of file permissions

--- drusaas.sh
+++ drusaas.sh
@@ -224,17 +224,6 @@ _EOF_
 # START SCRIPT
 ###################################################################
 
-# Got root?
-FILE="/tmp/out.$$"
-GREP="/bin/grep"
-#....
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-
 # Get input
 clear
 echo
