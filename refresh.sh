5762b187c97bfda1ed3b30f5fd75fda90d8bbf09
Fontaine Cook <fontaine.cook@pearance.com>
2011-05-03 20:43:08 -0400

    Adding refresh.sh script

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+clear
+
+curl -O https://github.com/Pearance/saasmaster/raw/master/saasmaster.sh && chmod 700 saasmaster.sh
+curl -O https://github.com/Pearance/saasmaster/raw/master/uninstall.sh && chmod 700 uninstall.sh
+
+echo "saasmaster.sh and uninstall.sh updated!"
+
