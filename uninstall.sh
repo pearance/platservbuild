aa638e657dcc91cdca7f63cea866bdbc2fc6b2a2
Fontaine Cook <fontaine.cook@pearance.com>
2011-05-03 22:40:45 -0400

    Added prompt for database name and removal of that database

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 clear
+
+read -p "This will refresh your build/install...\n\nEnter Database Name?" db
+
 rm -rf /srv/aegir
 crontab -r -u aegir
 userdel aegir --remove
@@ -8,6 +11,10 @@ userdel support --remove
 userdel itaine --remove
 rm /etc/apache2/conf.d/aegir.conf
 
-rm saasmaster.sh
-curl -O https://github.com/Pearance/saasmaster/raw/master/saasmaster.sh && chmod 700 saasmaster.sh && . saasmaster.sh
+echo -e "\n Removing Database"
+mysql -e 'drop database $db;'
+
+rm -r saasmaster
+echo -e "\n*** Clone Repository ***"
+git clone git://github.com/Pearance/saasmaster.git
 
